0da920cbf69fb8e1e26d8676f78a4d5093ed19cf
Matheus Ribeiro <matheus.gomes94@etec.sp.gov.br>
2019-09-20 11:54:26 -0300

    updating the api

--- init.py
+++ init.py
@@ -1,11 +1,9 @@
-#pyTelegramBotAPI
+#pip install pyTelegramBotAPI
 
 import telebot
 import time
 
-851084690
-
-TOKEN = 'Inserir token aqui'
+TOKEN = 'TOKEN AQUI'
 
 
 def escuta(messages):
@@ -17,14 +15,29 @@ def escuta(messages):
             PIZZAS              PREÇOS
             Portuguesa          R$50,00
             Marguerita          R$20,00
-            pythonguesa         R$40,00
+            Calabresa           R$40,00
+            Chocolight          R$45,00
+            Hot Dog             R$25,00
+            Doce Leite          R$40,00
+            Frango Catu         R$67,00
+            Mussabresa          R$34,00
+            /start
+            """)
+        if(m.text=='desconto' or m.text=="/desconto"):
+            print(chatid)
+            bot.send_message(chatid, """Desconto para você:
+            PIZZA$              DE$CONTO
+            Portuguesa          R$20,00
+            Marguerita          R$20,00
+            Frango Catu         R$27,00
+            Mussabresa          R$24,00
             /start
             """)
 
 
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
-def comando_teste(menssagem):
+def comando_start(menssagem):
     print(menssagem.from_user.first_name)
     bot.reply_to(menssagem,'Olá '+menssagem.from_user.first_name+""" Seja Bem vindo ao Bot da Pizzaria TecPizzas.
     Utilize o menu abaixo:
@@ -49,5 +62,5 @@ bot.set_update_listener(escuta) #Quando o usuário escrever algo ele chama a fun
 bot.polling()
 
 
-while True: # Don't let the main Thread end.
+while True: # DNão deixa a thread principal morrer
     pass
